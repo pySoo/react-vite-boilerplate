8fed8dad99ba634dd611ddc9a2c9a19b3a9c5537
pySoo <soopy368@gmail.com>
2024-02-19 23:47:52 +0900

    [feat] : react-query and styled-components initial setting

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,29 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
 
 import App from './App';
+import { GlobalStyle } from './styles/GlobalStyle';
+import { theme } from './styles/theme';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60 * 1000,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </React.StrictMode>,
 );
