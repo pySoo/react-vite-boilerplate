3b8fcd50fe124e23881d78b083e55b73aa497a1e
pySoo <soopy368@gmail.com>
2024-02-20 00:02:06 +0900

    [feat] : set route provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
+import { RouterProvider } from 'react-router-dom';
+
+import { router } from './routes';
+
 function App() {
-  return <div>Hello World!</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
